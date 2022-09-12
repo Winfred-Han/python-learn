d74d70250a3ef1684752def2fd0e9a14874fb374
Winfred-Han <356403252@qq.com>
2022-09-12 20:15:22 +0800

    元组学习

--- yuanzu/tuple_1.py
+++ yuanzu/tuple_1.py
@@ -21,3 +21,32 @@ print(f"t5的类型是：{type(t5)},内容是{t5}")
 # 下标索引去取出内容
 num=t5[1][2]
 print(f"从嵌套元组中取出的数据是:{num}")
+
+# 元组的操作：index查找方法
+t6=("传智教育","黑马程序员","python")
+index=t6.index("黑马程序员")
+print(f"元组t6中查找黑马程序员，的下标是：{index}")
+
+# 元组的操作：count统计方法
+t7=("传智教育","黑马程序员","黑马程序员","黑马程序员","python")
+count=t7.count("黑马程序员")
+print(f"元组t7中统计黑马程序员的数量是：{count}")
+
+# 元组的操作：count统计方法
+t8=("传智教育","黑马程序员","黑马程序员","黑马程序员","python")
+count=len(t8)
+print(f"元组t8中统计元素的数量是：{count}")
+# 元组的遍历：while
+index=0
+while index<len(t8):
+    print(f"元组的元素有：{t8[index]}")
+    index+=1
+for i in t8:
+    print(f"for元组中的元素有：{i}")
+# 修改元素中的内容
+# t8[0]='sdfsdf'  这样内容是不可以修改的
+t9=[1,2,["itheima","itcast"]]
+print(f"t9的内容是：{t9}")
+t9[2][0]="黑马程序员"
+t9[2][1]="传智教育"
+print(f"t9的内容是：{t9}")
