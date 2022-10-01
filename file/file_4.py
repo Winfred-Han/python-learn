b36196e4cea2d81cf56a53d601622ed366cd1d3a
356403252@qq.com <356403252@qq.com>
2022-10-01 23:57:01 +0800

    with open

--- file/file_4.py
+++ file/file_4.py
@@ -1,7 +1,8 @@
-
+import os
 
 
 # 向文件中追加内容
-f=open("E:/file1/追加文件.txt","a",encoding="utf-8")
-f.write("5555")
-f.close()
+# f=open("E:/file1/追加文件.txt","a",encoding="utf-8")
+with open("E:/file1/追加文件.txt", 'a',encoding="utf-8") as f:
+    f.write("5555")
+    f.flush()
